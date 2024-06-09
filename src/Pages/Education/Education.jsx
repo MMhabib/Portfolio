152d9b197f8e2f3e009bdc3291fdb4ac9cae9863
Habib <habibleo861@gmail.com>
2024-06-09 12:57:38 +0600

    Education

--- src/Pages/Education/Education.jsx
+++ src/Pages/Education/Education.jsx
@@ -1,11 +1,88 @@
-
-
 const Education = () => {
-    return (
-        <div>
-            <p>habib</p>
+  return (
+    <div>
+      <div className="card lg:card-side bg-base-100 shadow-xl m-6 mx-14">
+        <figure className="m-6 p-2">
+          <img className="border rounded-md"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfdHdcg6tnvQq-V9OPo2W7A0e0mt1dtBmJxQ&s"
+            alt="Album"
+          />
+        </figure>
+        <div className="card-body lg:ml-96">
+          <h1 className="card-title">
+            Bangladesh Army International University of Science and Technology
+          </h1>
+          <h3>Cumilla, Bangladesh</h3>
+          <h2 className="font-semibold">Bachelor of Science in Computer Science and Engineering (BSc)</h2>
+          <p>March 2020 - March 2024</p>
+          {/* <div className="card-actions justify-end">
+            <button className="btn btn-primary">Listen</button>
+          </div> */}
         </div>
-    );
+
+      </div>
+      <div className="card lg:card-side bg-base-100 shadow-xl m-6 mx-14">
+        <figure className="m-6 p-2">
+          <img className="border rounded-md"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIZ20OFrPchAgPvKiatvajIBaleDPK8iqtwA&s"
+            alt="Album"
+          />
+        </figure>
+        <div className="card-body lg:ml-96">
+          <h1 className="card-title">
+          Cumilla Government College
+          </h1>
+          <h3>Cumilla, Bangladesh</h3>
+          <h2 className="font-semibold">Higher Secondary Certificate (HSC)</h2>
+          <p>june 2016- April 2018</p>
+          {/* <div className="card-actions justify-end">
+            <button className="btn btn-primary">Listen</button>
+          </div> */}
+        </div>
+
+      </div>
+      <div className="card lg:card-side bg-base-100 shadow-xl m-6 mx-14">
+        <figure className="m-6 p-2">
+          <img className="border rounded-md"
+            src="https://upload.wikimedia.org/wikipedia/en/c/c6/Comilla_Zilla_School.jpg"
+            alt="Album"
+          />
+        </figure>
+        <div className="card-body lg:ml-96">
+          <h1 className="card-title">
+          Cumilla Zilla School
+          </h1>
+          <h3>Cumilla, Bangladesh</h3>
+          <h2 className="font-semibold">Secondary School Certificate (SSC)</h2>
+          <p>March 2020 - March 2024</p>
+          {/* <div className="card-actions justify-end">
+            <button className="btn btn-primary">Listen</button>
+          </div> */}
+        </div>
+
+      </div>
+      <div className="card lg:card-side bg-base-100 shadow-xl m-6 mx-14">
+        <figure className="m-6 p-2">
+          <img className="border rounded-md"
+            src="https://upload.wikimedia.org/wikipedia/en/c/c6/Comilla_Zilla_School.jpg"
+            alt="Album"
+          />
+        </figure>
+        <div className="card-body lg:ml-96">
+          <h1 className="card-title">
+          Gulbagicha Model Government Primary School
+          </h1>
+          <h3>Cumilla, Bangladesh</h3>
+          <h2 className="font-semibold">Primary School Certificate (PSC)</h2>
+          <p>March 2020 - March 2024</p>
+          {/* <div className="card-actions justify-end">
+            <button className="btn btn-primary">Listen</button>
+          </div> */}
+        </div>
+
+      </div>
+    </div>
+  );
 };
 
 export default Education;
