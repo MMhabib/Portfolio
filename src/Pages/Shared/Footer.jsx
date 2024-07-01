01ee94b6a8a78c378019d4e0d503b468a65fc1da
Habib <habibleo861@gmail.com>
2024-07-01 14:57:09 +0600

    Footer

--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -7,17 +7,18 @@ const Footer = () => {
           
             <footer className="footer p-10  mt-1 bg-neutral text-neutral-content">
             
-            <div className='footer-container p-5'>
-            <p className="">All copyright reserved - M.M HABIB
-            </p>
+            <div className='footer-container p-2  ml-20'>
+            
                 <h1 className="text-lg">📧 habib.se56@gmail.com</h1>
+<br/>
                 <h2 className="text-lg">📞 +880 10709004842 </h2>
+<br/>
                 <h2 className="text-lg">🏠 Cumilla 3500, BD</h2>
         
             </div>
 
-  <nav>
-    <h6 className="footer-title">Social</h6> 
+  <nav className=" justify-self-end"> 
+    <h6 className="footer-title m-6 ">Social</h6> 
     <div className="grid grid-flow-col gap-4">
     <SocialIcon url="https://www.facebook.com/habibczs" />
     <SocialIcon url="https://x.com/1051Habib" />
